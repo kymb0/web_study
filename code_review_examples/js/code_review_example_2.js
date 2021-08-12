2765ddbc3d3313e0c9762996555dfcdce892452d
kymb0 <55473161+kymb0@users.noreply.github.com>
2021-08-12 14:53:42 +0930

    Update code_review_example_2.js

--- code_review_examples/js/code_review_example_2.js
+++ code_review_examples/js/code_review_example_2.js
@@ -6,11 +6,11 @@
     where.push(param+"=?") // push all parameters sent in get request into 'where' array with "=?" appended (So where would look like [number=?,string=?]'?' is a placeholder in SQL)
     data.push(req.query[param]); // push all parameter values into data (so now data looks like [1, 'hello world'])
   }
-  query+=where.join(" AND "); //concatenate query with the where array, so now it looks like "SELECT * FROM users WHERE number=?, string=?"
-  db.query(query, data, (err,rows) => { //now we send our query to the database
+  query+=where.join(" AND "); //concatenate query with the where array, joining the contents with ' AND '
+  db.query(query, data, (err,rows) => { //now we send our query to the database, along with the data in a prepared statement. 
     if(err) return res.send(JSON.stringify({}));
     return res.send(JSON.stringify(rows));
   });  
 });
 
-// why is the above vulnerable? 
+// why is the above vulnerable? although the query was correctly constructed, there is not an allowed list of parameters, indicating that we can achieve injection not by the value of the params, but thruogh the params themselves
